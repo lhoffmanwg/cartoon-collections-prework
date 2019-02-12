276a4b8ffc099f1b9f24609c9b92483f412863fa
Laura Hoffman <lhoffmanwg@gmail.com>
2019-02-12 20:21:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,11 +21,9 @@ end
 
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.map { |cheese| 
-    if "true"
-     new_array=new_array + strings.include?(cheese)
-  else
-    new_array = " "
+  cheese_types.map { |cheese|
+  if strings.include?(cheese)
+    return "true"
   end  
    }
   
